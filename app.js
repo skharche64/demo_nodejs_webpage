468985322effccad7e9c4ea023dda5abb2a89312
skharche64 <162728930+skharche64@users.noreply.github.com>
2024-04-09 13:51:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get('/', (req,res) => {
   
 });
 
+app.get('/about', (req,res) => {
+    res.sendFile(path.join(__dirname+'/about.html'));
+   
+  });
 
 app.listen(8081, () => {
     console.log('Listening on port 8081');
